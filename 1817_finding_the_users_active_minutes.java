7c44b596d5e133a6357b987b713c607b290c9cf2
Dayue Bai <dayuebai@gmail.com>
2021-04-04 13:07:10 -0500

    update answer

--- 1817_finding_the_users_active_minutes.java
+++ 1817_finding_the_users_active_minutes.java
@@ -3,9 +3,7 @@ class Solution {
         int[] ans = new int[k];
         HashMap<Integer, Set<Integer>> logMap = new HashMap<>();
         for (int[] l : logs) {
-            if (!logMap.containsKey(l[0])) {
-                logMap.put(l[0], new HashSet<Integer>());
-            }
+            logMap.putIfAbsent(l[0], new HashSet<Integer>());
             logMap.get(l[0]).add(l[1]);
         }
         

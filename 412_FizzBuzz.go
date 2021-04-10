78ce8f2efbd3cc7fe80d17befc222c6fe36384ba
Dayue Bai <dayuebai@gmail.com>
2021-04-10 15:41:35 -0500

    412

---
+++ 412_FizzBuzz.go
@@ -0,0 +1,15 @@
+func fizzBuzz(n int) []string {
+    ans := make([]string, n)
+    for i := 1; i <= n; i++ {
+        if i % 3 == 0 && i % 5 == 0 {
+            ans[i-1] = "FizzBuzz"
+        } else if i % 3 == 0 {
+            ans[i-1] = "Fizz"
+        } else if i % 5 == 0 {
+            ans[i-1] = "Buzz"
+        } else {
+            ans[i-1] = strconv.Itoa(i)
+        }
+    }
+    return ans
+}

ad47938e03be9f8f459dd7625c21fe9723b589f2
Dayue Bai <dayuebai@gmail.com>
2021-04-11 17:15:38 -0500

    1822

---
+++ 1822_sign_of_the_product_of_an_array.go
@@ -0,0 +1,15 @@
+func arraySign(nums []int) int {
+    neg := 0
+    for _,n := range nums {
+        if n == 0 {
+            return 0
+        } else if n < 0 {
+            neg++
+        }
+    }
+    if neg % 2 == 0 {
+        return 1
+    } else {
+        return -1
+    }
+}
